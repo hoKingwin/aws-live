eb26467997308f22ef4f277aba99a34dffe4882b
KinWeng <32710027+hoKingwin@users.noreply.github.com>
2023-08-28 10:48:11 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.covwe6neufot.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "hokinweng-employee"
+customregion = "us-east-1"
+
